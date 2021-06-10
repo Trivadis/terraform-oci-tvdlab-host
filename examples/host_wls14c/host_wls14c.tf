afe983711a901dd85eac5c5f05bacbb729f51d70
Stefan Oehrli <stefan.oehrli@trivadis.com>
2021-06-10 11:50:31 +0200

    update minimal release to 0.0.4

--- examples/host_wls14c/host_wls14c.tf
+++ examples/host_wls14c/host_wls14c.tf
@@ -17,7 +17,7 @@
 # - ADD VCM Module -------------------------------------------------------------
 module "tvdlab-wls14c" {
   source  = "Trivadis/tvdlab-host/oci"
-  version = ">=0.0.3"
+  version = ">=0.0.4"
 
   # - Mandatory Parameters -----------------------------------------------------
   region              = var.region                          # The OCI region where resources will be created

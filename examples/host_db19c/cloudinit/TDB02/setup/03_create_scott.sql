b530605a545509187bd0ddc3e7194cd14489702e
Stefan Oehrli <stefan.oehrli@trivadis.com>
2021-06-10 11:46:13 +0200

    Update scripts for TDB02

---
+++ examples/host_db19c/cloudinit/TDB02/setup/03_create_scott.sql
@@ -0,0 +1,17 @@
+----------------------------------------------------------------------------
+--  Trivadis AG, Infrastructure Managed Services
+--  Saegereistrasse 29, 8152 Glattbrugg, Switzerland
+----------------------------------------------------------------------------
+--  Name......: 03_create_scott.sql
+--  Author....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
+--  Editor....: Stefan Oehrli
+--  Date......: 2021.01.11
+--  Revision..:  
+--  Purpose...: Script to create the SCOTT schema
+--  Notes.....:  
+--  Reference.: SYS (or grant manually to a DBA)
+--  License...: Apache License Version 2.0, January 2004 as shown
+--              at http://www.apache.org/licenses/
+----------------------------------------------------------------------------
+@?/rdbms/admin/utlsampl.sql
+-- EOF ---------------------------------------------------------------------
